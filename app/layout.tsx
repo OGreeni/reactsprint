86fc0999c9796e03e531539379849b120fbcdd6a
OGreeni <omri.green1@gmail.com>
2023-01-08 18:01:45 -0500

    basic configuration and setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
-import './globals.css'
+/* eslint-disable @next/next/no-page-custom-font */
+import "@styles/global.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;700;900&display=swap"
+        rel="stylesheet"
+      />
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
