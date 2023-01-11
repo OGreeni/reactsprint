e07affc6a84492b5158b19b4459e53b17ae2dd15
OGreeni <omri.green1@gmail.com>
2023-01-11 00:22:59 -0500

    feat: redirect guest users to login on like button click

--- app/(root)/challenge-board/[slug]/like-counter.tsx
+++ app/(root)/challenge-board/[slug]/like-counter.tsx
@@ -2,6 +2,10 @@
 import React from 'react';
 import axios from 'axios';
 
+import { SignedIn, SignedOut } from '@clerk/nextjs/app-beta/client';
+import Login from '@components/auth/login-button';
+import LoginButton from '@components/auth/login-button';
+import RedirectToSignInWrapper from '@components/auth/redirect-to-sign-in-wrapper';
 import { StyledButton } from '@components/styled';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
@@ -63,31 +67,53 @@ export default function LikeCounter({ challengeId }: Props) {
 
   return (
     <>
-      <StyledButton
-        onClick={() => {
-          if (query?.data.userData.isLiked) {
-            mutation.mutate({ increment: false });
-          } else {
-            mutation.mutate({ increment: true });
-          }
-        }}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill={query?.data?.userData?.isLiked ? 'white' : 'none'}
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="h-10 w-10"
+      <SignedIn>
+        <StyledButton
+          onClick={() => {
+            if (query?.data.userData.isLiked) {
+              mutation.mutate({ increment: false });
+            } else {
+              mutation.mutate({ increment: true });
+            }
+          }}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
-          />
-        </svg>
-      </StyledButton>
-      <span className="text-3xl font-bold">{query.data?.likes}</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill={query?.data?.userData?.isLiked ? 'white' : 'none'}
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-10 w-10"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
+            />
+          </svg>
+        </StyledButton>
+        <span className="text-3xl font-bold">{query.data?.likes}</span>
+      </SignedIn>
+      <SignedOut>
+        <StyledButton>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill={'none'}
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-10 w-10"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
+            />
+          </svg>
+          <LoginButton />
+        </StyledButton>
+        <span className="text-3xl font-bold">{query.data?.likes}</span>
+      </SignedOut>
     </>
   );
 }
